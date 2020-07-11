56c243d19300af8bd2e69b2ca686da914f466ce3
Chinmay Garde <chinmaygarde@google.com>
2020-07-10 19:48:47 -0700

    Fix incorrectly staged CMake custom command for processing grammar and add error messages.

--- tools/grammar.cmake
+++ tools/grammar.cmake
@@ -4,10 +4,14 @@ endif()
 set(__grammar INCLUDED)
 
 function(add_lexer TARGET FLEX_FILE_PATH)
+  find_program (FLEX_PROGRAM flex)
+  if(NOT FLEX_PROGRAM)
+    message(FATAL_ERROR "Could not find flex (>=2.6.4) on the host.")
+  endif()
   get_filename_component(FLEX_FILE_PATH ${FLEX_FILE_PATH} ABSOLUTE)
   add_custom_command(
     OUTPUT ${ARGN}
-    COMMAND /Users/chinmaygarde/bin/flex --noline ${FLEX_FILE_PATH}
+    COMMAND ${FLEX_PROGRAM} --noline ${FLEX_FILE_PATH}
     DEPENDS ${FLEX_FILE_PATH}
   )
   target_sources(${TARGET}
@@ -17,10 +21,14 @@ function(add_lexer TARGET FLEX_FILE_PATH)
 endfunction()
 
 function(add_parser TARGET BISON_FILE_PATH)
+  find_program (BISON_PROGRAM bison)
+  if(NOT BISON_PROGRAM)
+    message(FATAL_ERROR "Could not find bison (>=3.6.4) on the host.")
+  endif()
   get_filename_component(BISON_FILE_PATH ${BISON_FILE_PATH} ABSOLUTE)
   add_custom_command(
     OUTPUT ${ARGN}
-    COMMAND /Users/chinmaygarde/bin/bison -Werror ${BISON_FILE_PATH}
+    COMMAND ${BISON_PROGRAM} -Werror ${BISON_FILE_PATH}
     DEPENDS ${BISON_FILE_PATH}
   )
   target_sources(${TARGET}
