953680e7c2f2d2573d45312160ffdecdb3b47596
Chinmay Garde <chinmaygarde@gmail.com>
2020-07-14 23:14:12 -0700

    Minor error handling while reading and writing files.

--- source/file.cc
+++ source/file.cc
@@ -4,19 +4,24 @@
 #include "file.h"
 
 #include <fstream>
+#include <iostream>
 #include <sstream>
 
 namespace epoxy {
 
 std::optional<std::string> ReadFileAsString(const std::string& file_path) {
   std::ifstream file_stream;
-  file_stream.open(file_path, std::ifstream ::in);
-  if (!file_stream.good()) {
+  file_stream.open(file_path, std::ifstream::in);
+  if (file_stream.fail()) {
+    std::cerr << "Could not open " << file_path << " for reading." << std::endl;
     return std::nullopt;
   }
   std::stringstream stream;
   stream << file_stream.rdbuf();
-  file_stream.clear();
+  if (!file_stream.good()) {
+    std::cerr << "Could not read the whole file " << file_path << std::endl;
+    return std::nullopt;
+  }
   return stream.str();
 }
 
@@ -24,11 +29,15 @@ bool OverwriteFileWithStringData(const std::string& file_path,
                                  std::string data) {
   std::ofstream file_stream;
   file_stream.open(file_path, std::ofstream::out | std::ofstream::trunc);
-  if (!file_stream.good()) {
+  if (file_stream.fail()) {
+    std::cerr << "Could not open " << file_path << " for writing." << std::endl;
     return false;
   }
   file_stream << data;
-  file_stream.close();
+  if (!file_stream.good()) {
+    std::cerr << "Could not write the whole file " << file_path << std::endl;
+    return false;
+  }
   return true;
 }
 
