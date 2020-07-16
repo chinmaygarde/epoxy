980df85ca1c34e9f29b4779cba7e98cd5d467ea3
Chinmay Garde <chinmaygarde@gmail.com>
2020-07-16 01:17:19 -0700

    Unify example and root CMake projects.

--- example/tools/epoxy.cmake
+++ example/tools/epoxy.cmake
@@ -4,11 +4,6 @@ endif()
 set(__epoxy INCLUDED)
 
 function(epoxy TARGET EPOXY_TEMPLATE_PATH EPOXY_IDL_PATH OUTPUT_FILE_NAME)
-  find_program (EPOXY_PROGRAM epoxy)
-  if(NOT EPOXY_PROGRAM)
-    message(FATAL_ERROR "Could not find flex on the host in the default search path.")
-  endif()
-
   get_filename_component(EPOXY_TEMPLATE_PATH ${EPOXY_TEMPLATE_PATH} ABSOLUTE)
   get_filename_component(EPOXY_IDL_PATH ${EPOXY_IDL_PATH} ABSOLUTE)
 
@@ -16,8 +11,8 @@ function(epoxy TARGET EPOXY_TEMPLATE_PATH EPOXY_IDL_PATH OUTPUT_FILE_NAME)
 
   add_custom_command(
     OUTPUT "${OUTPUT_FILE_PATH}"
-    COMMAND "${EPOXY_PROGRAM}" --output "${OUTPUT_FILE_PATH}" --idl "${EPOXY_IDL_PATH}" --template-file "${EPOXY_TEMPLATE_PATH}"
-    DEPENDS "${EPOXY_PROGRAM}" "${EPOXY_IDL_PATH}" "${EPOXY_TEMPLATE_PATH}"
+    COMMAND epoxy --output "${OUTPUT_FILE_PATH}" --idl "${EPOXY_IDL_PATH}" --template-file "${EPOXY_TEMPLATE_PATH}"
+    DEPENDS "${EPOXY_IDL_PATH}" "${EPOXY_TEMPLATE_PATH}"
   )
 
   target_sources(${TARGET} PUBLIC "${OUTPUT_FILE_PATH}")
